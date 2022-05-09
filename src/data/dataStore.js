4fa14a4b67748b1cbc969a7aaceb151bd652e3ca
Nova+ <cszonalt@gmail.com>
2022-05-10 01:42:23 +0300

    using ttstats internal proxy as fallback

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -28,7 +28,43 @@ export const serversAtom = atom({
 	}, {}),
 });
 
-export const fetchServer = (server, setServer) => {
+const cancellableJSONFetch = async ({ cancelController, url, cancelControllerTimeout }) => {
+	cancelControllerTimeout = setTimeout(() => {
+		cancelController.abort();
+	}, 5000);
+
+	const res = await fetch(url, {
+		signal: cancelController.signal,
+		redirect: "error",
+	});
+
+	if (res.status === 404) {
+		throw new Error("offline");
+	} else {
+		return res.json();
+	}
+};
+
+const parseStatusJSON = async ({ res, setServer, server }) => {
+	if (!("server" in res)) throw new Error("offline");
+
+	for (let i = 0; i < res.players.length; i++) {
+		if (res.players[i][5] === "") res.players[i][5] = "Unemployed";
+	}
+
+	setServer((s) => ({
+		...s,
+		[server.endpoint]: {
+			...server,
+			loaded: true,
+			playersData: res.players,
+			serverData: res.server,
+			lastUpdate: Date.now(),
+		},
+	}));
+};
+
+export const fetchServer = async (server, setServer) => {
 	// set server loading status
 	setServer((s) => ({
 		...s,
@@ -39,97 +75,70 @@ export const fetchServer = (server, setServer) => {
 	}));
 
 	const cancelController = new AbortController();
-	const cancelControllerTimeout = setTimeout(() => {
-		cancelController.abort();
-	}, 6000);
+	let cancelControllerTimeout;
+	let success = false;
 
-	fetch(`https://tycoon-${server.endpoint}.users.cfx.re/status/widget/players.json`, {
-		signal: cancelController.signal,
-		redirect: "error",
-	})
-		.then((res) => {
-			if (res.status === 404) {
-				throw new Error("offline");
-			} else {
-				return res.json();
-			}
-		})
-		.then((res) => {
-			if (!("server" in res)) throw new Error("offline");
+	//fetch fivem reverse proxy
+	try {
+		const res = await cancellableJSONFetch({ cancelController, cancelControllerTimeout, url: `https://tycoon-${server.endpoint}.users.cfx.re/status/widget/players.json` });
+		await parseStatusJSON({ res, setServer, server });
+		success = true;
+	} catch (err) {}
 
-			for (let i = 0; i < res.players.length; i++) {
-				if (res.players[i][5] === "") res.players[i][5] = "Unemployed";
-			}
+	//else fetch ttstats reverse proxy
+	try {
+		clearTimeout(cancelControllerTimeout);
+		if (success) return;
+		const res = await cancellableJSONFetch({ cancelController, cancelControllerTimeout, url: `https://novaplus-api.herokuapp.com/status/${server.endpoint}` });
+		await parseStatusJSON({ res, setServer, server });
+		success = true;
+	} catch (err) {}
 
-			setServer((s) => ({
-				...s,
-				[server.endpoint]: {
-					...server,
-					loaded: true,
-					playersData: res.players,
-					serverData: res.server,
-					lastUpdate: Date.now(),
+	//else fetch fivem server status api
+	try {
+		clearTimeout(cancelControllerTimeout);
+		if (success) return;
+		const res = await cancellableJSONFetch({ cancelController, cancelControllerTimeout, url: `https://servers-frontend.fivem.net/api/servers/single/${server.endpoint}` });
+
+		if (!("Data" in res)) throw new Error("offline");
+		const data = res["Data"];
+
+		setServer((s) => ({
+			...s,
+			[server.endpoint]: {
+				...server,
+				loaded: true,
+				playersData: data.players.map((player) => [player.name || "?", -1, "?", null, false, "?", false]),
+				serverData: {
+					limit: data?.["sv_maxclients"] || 32,
+					beta: "",
+					dxp: [false],
+					// "dxp": [true,"FAKENAME",5000,5000,5000],
+					uptime: data?.["vars"]?.["Uptime"] || "1m",
+					region: "?",
+					number: "?",
+					name: "",
+					motd: "",
 				},
-			}));
+				lastUpdate: Date.now(),
+			},
+		}));
 
-			//if tt api is down use fivem api to fetch the data
-		})
-		.catch(async () => {
-			clearTimeout(cancelControllerTimeout);
-			setTimeout(() => {
-				cancelController.abort();
-			}, 5000);
+		success = true;
+	} catch (err) {}
 
-			fetch(`https://servers-frontend.fivem.net/api/servers/single/${server.endpoint}`, {
-				signal: cancelController.signal,
-				redirect: "error",
-			})
-				.then((res) => {
-					if (res.status === 404) {
-						throw new Error("offline");
-					} else {
-						return res.json();
-					}
-				})
-				.then((res) => {
-					if (!("Data" in res)) throw new Error("offline");
-					const data = res["Data"];
-
-					setServer((s) => ({
-						...s,
-						[server.endpoint]: {
-							...server,
-							loaded: true,
-							playersData: data.players.map((player) => [player.name || "?", -1, "?", null, false, "?", false]),
-							serverData: {
-								limit: data?.["sv_maxclients"] || 32,
-								beta: "",
-								dxp: [false],
-								// "dxp": [true,"FAKENAME",5000,5000,5000],
-								uptime: data?.["vars"]?.["Uptime"] || "1m",
-								region: "?",
-								number: "?",
-								name: "",
-								motd: "",
-							},
-							lastUpdate: Date.now(),
-						},
-					}));
-
-					//server is offline
-				})
-				.catch(() => {
-					setServer((s) => ({
-						...s,
-						[server.endpoint]: {
-							...server,
-							loaded: true,
-							error: true,
-							lastUpdate: Date.now(),
-						},
-					}));
-				});
-		});
+	//else all fetches failed, set status to offline
+	clearTimeout(cancelControllerTimeout);
+	if (success) return;
+	setServer((s) => ({
+		...s,
+		[server.endpoint]: {
+			...server,
+			loaded: true,
+			error: true,
+			lastUpdate: Date.now(),
+		},
+	}));
 };
 
 export const fetchAllServers = (servers, setServers) => {
