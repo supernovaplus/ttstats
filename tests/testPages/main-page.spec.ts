0de488a0b8a9580e2920d20d7441a8a41a0c0308
Nova+ <cszonalt@gmail.com>
2023-05-20 19:40:16 +0300

    connect via endpoint textarea

--- tests/testPages/main-page.spec.ts
+++ tests/testPages/main-page.spec.ts
@@ -1,5 +1,3 @@
-
-
 import { test, expect } from '@playwright/test';
 import { testingFindText } from '../../src/controllers/misc.ts';
 
@@ -7,5 +5,9 @@ test('test main page / server list', async ({ page }) => {
   await page.goto('http://localhost:5173/');
   await page.waitForTimeout(1500);
   const content = await page.content();
-  testingFindText(content, ['NY-2 (Beta)', 'Connect', 'skill boost', 'Info', 'DXP'], 'main page / server list');
+  testingFindText(
+    content,
+    ['NY-2 (Beta)', 'Connect', 'skill boost', 'Info', 'DXP'],
+    'main page / server list'
+  );
 });
